f99b2d60172adb993dc6d3d606108a41132cecaa
Renaud Zigmann <renaud.zigmann@xsalto.com>
2015-12-22 11:06:04 +0100

    version 0.9

---
+++ lang/fr.js
@@ -0,0 +1,102 @@
+(function($){
+	$.fn.imageEditor.prototype.lang = {
+
+
+		////////
+		// Menu
+		////////
+
+
+		title:				"Image Editor",
+		crop:				"Recadrer",
+		filters:			"Filtres",
+		image_process:			"Traitement d'image",
+		compare:			"Comparer",
+		reset:				"Tout annuler",
+		upload_button:			"Sauvegarder sur le serveur",
+		download_button:		"Télécharger l'image",
+		close_button:			"Quitter",
+		validate_button:		"Valider",
+		cancel_button:			"Annuler",
+		close_msg:			"Quitter sans sauvegarder sur le serveur ?",
+		close_uploading_msg:		"Une mise en ligne est en cours, voulez-vous l'arrêter et quitter ?",
+		loading_image_msg:		"Chargement...",
+		error_loading_image_msg:	"L'image n'a pas été chargé",
+		error_msg:			"Erreur",
+		error_glfx_load_msg:		"glfx.js n'a pas chargé",
+		error_glfx_support_msg:		"Désolé, ce navigateur ne supporte pas WebGL",
+		checkbox_preview:		"Prévisualiser sur la taille reel",
+		
+
+		////////
+		// Traitements
+		////////
+
+
+		'brightnessContrast':		"Luminosité / Contrast",
+		'hueSaturation':		"Teinte / Saturation",
+		'tiltShift':			"Effet de maquette",
+		'vibrance':			"Vibrance",
+		'denoise':			"Réducteur de bruit",
+		'noise':			"Bruit",
+		'unsharpMask':			"Accentuation",
+		'sepia':			"Sépia",
+		'vignette':			"Vignette",
+		'zoomBlur':			"Zoom Flouté",
+		'triangleBlur':			"Flou triangulaire",
+		'lensBlur':			"Flou de l'objectif",
+		'swirl':			"Tourbillon",
+		'bulgePinch':			"Renfler / Pincer",
+		'perspective':			"Perspective",
+		'ink':				"Ink",
+		'edgeWork':			"Edge Work",
+		'hexagonalPixelate':		"Pixelisation hexagonale",
+		'dotScreen':			"Dot Screen",
+		'colorHalftone':		"Color Halftone",
+
+
+		////////
+		// Traitements -> Sliders
+		////////
+
+
+		'brightness':			"Luminausitée",
+		'contrast':			"Contrast",
+		'hue':				"Teinte",
+		'saturation':			"Saturation",
+		'blurRadius':			"Flou",
+		'gradientRadius':		"Epaisseur",
+		'angle':			"Rotation",
+		'radius':			"Rayon",
+		'strength':			"Force",
+		'size':				"Taille",
+		'scale':			"Echelle",
+		'amount':			"Quantité",
+		'exponent':			"Exposant",
+
+
+		////////
+		// Filtres
+		////////
+
+		"vintage":			"vintage",
+		"lomo":				"lomo",
+		"clarity":			"clarity",
+		"sinCity":			"sinCity",
+		"sunrise":			"sunrise",
+		"crossProcess":			"crossProcess",
+		"orangePeel":			"orangePeel",
+		"love":				"love",
+		"grungy":			"grungy",
+		"jarques":			"jarques",
+		"pinhole":			"pinhole",
+		"oldBoot":			"oldBoot",
+		"glowingSun":			"glowingSun",
+		"hazyDays":			"hazyDays",
+		"herMajesty":			"herMajesty",
+		"nostalgia":			"nostalgia",
+		"hemingway":			"hemingway",
+		"concentrate":			"concentrate",
+
+	};
+}(jQuery));
